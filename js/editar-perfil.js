4d79c20f173b6e4661d5b85231c177aa0c0c6588
rarenexusteam <rarenexustcg@gmail.com>
2026-07-16 22:40:49 +0100

    Update editar-perfil.js

--- js/editar-perfil.js
+++ js/editar-perfil.js
@@ -1,1 +1,44 @@
+import { auth } from "./firebase.js";
 
+import {
+    onAuthStateChanged,
+    updateProfile
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
+const nome = document.getElementById("nome");
+const salvar = document.getElementById("salvar");
+
+onAuthStateChanged(auth, (user) => {
+
+    if (!user) {
+        window.location.href = "login.html";
+        return;
+    }
+
+    nome.value = user.displayName || "";
+
+});
+
+salvar.addEventListener("click", async () => {
+
+    const user = auth.currentUser;
+
+    if (!user) return;
+
+    try {
+
+        await updateProfile(user, {
+            displayName: nome.value
+        });
+
+        alert("Perfil atualizado com sucesso!");
+
+        window.location.href = "perfil.html";
+
+    } catch (erro) {
+
+        alert("Erro: " + erro.message);
+
+    }
+
+});
