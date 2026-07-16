b8f05818b16afab02ef704e73707a0b308fdf872
rarenexusteam <rarenexustcg@gmail.com>
2026-07-16 03:20:07 +0100

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,26 @@
+// ==========================
+// Menu da Conta
+// ==========================
 
+const accountMenu = document.getElementById("accountMenu");
+const profileButton = document.querySelector('a[href="login.html"]');
+
+if (profileButton && accountMenu) {
+
+    profileButton.addEventListener("click", function (e) {
+        e.preventDefault();
+        accountMenu.classList.toggle("active");
+    });
+
+    document.addEventListener("click", function (e) {
+
+        if (
+            !accountMenu.contains(e.target) &&
+            !profileButton.contains(e.target)
+        ) {
+            accountMenu.classList.remove("active");
+        }
+
+    });
+
+}
