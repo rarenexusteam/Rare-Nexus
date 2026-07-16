e061fb97d6a2a69f89573f0a2522b1c119cfda61
rarenexusteam <rarenexustcg@gmail.com>
2026-07-16 02:49:56 +0100

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,1 +1,24 @@
+import { auth } from "./firebase.js";
+import {
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
 
+const loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+
+    try {
+        await signInWithEmailAndPassword(auth, email, senha);
+
+        alert("Login realizado com sucesso!");
+
+        window.location.href = "index.html";
+
+    } catch (erro) {
+        alert("Erro: " + erro.message);
+    }
+});
